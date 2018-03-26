e8b583e7ade38e212fde443614712bc8f727041a
Sidhant Gandhi <sdgandhi@users.noreply.github.com>
2018-03-26 14:24:31 -0700

    Add app icon to demo video

--- src/DemoVideo.js
+++ src/DemoVideo.js
@@ -6,6 +6,12 @@ class DemoVideo extends Component {
   render() {
     return (
       <div className="demo-video">
+        <img
+          className="center-block"
+          src="https://imgur.com/PRYIKU9.png"
+          style={{ height: "128px", width: "auto", paddingTop: "3rem" }}
+          alt="Arra app icon"
+        />
         <div className="phone-frame center-block">
           <video autoplay loop muted controls preload>
             <source src="https://imgur.com/RG2p6ZN.mp4" />
