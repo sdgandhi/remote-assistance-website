dda3743559aa5df31a7d311d0b769be0345a657a
Sidhant Gandhi <sdgandhi@users.noreply.github.com>
2018-03-26 14:40:41 -0700

    Add poster to demo video

--- src/DemoVideo.js
+++ src/DemoVideo.js
@@ -13,7 +13,7 @@ class DemoVideo extends Component {
           alt="Arra app icon"
         />
         <div className="phone-frame center-block">
-          <video autoplay loop muted controls preload>
+          <video autoplay loop muted controls preload poster="https://imgur.com/mKpuXGr.jpg">
             <source src="https://imgur.com/RG2p6ZN.mp4" />
           </video>
         </div>
