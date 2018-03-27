98db9c7149da2ecb9519640fd373deec6c6527bd
Sidhant Gandhi <sidhant.gandhi@gmail.com>
2018-03-27 11:14:50 -0700

    Add google analytics

--- src/index.js
+++ src/index.js
@@ -8,11 +8,28 @@ import Contact from "./Contact";
 import Footer from "./Footer";
 import Beta from "./Beta";
 import "./index.css";
+import Config from "./config.json";
+import ReactGA from 'react-ga';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		const analyticsCode = Config.google_analytics;
+		ReactGA.initialize(analyticsCode, {
+			debug: false
+		});
+	}
+
   render() {
+	  const logPageView = () => {
+		  ReactGA.pageview(window.location.pathname + window.location.search);
+		  return null;
+	  };
+
     return (
       <div>
+	      <Route component={logPageView} />
         <ArraNavbar />
         <Switch>
           <Route exact path="/" component={Home} />
