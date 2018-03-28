8a2b74e096e0ed8242f9b5b6261a0803e1cdbf5e
Sidhant Gandhi <sdgandhi@users.noreply.github.com>
2018-03-27 18:21:03 -0700

    Use simpler google analytics tracking

--- src/index.js
+++ src/index.js
@@ -8,28 +8,13 @@ import Contact from "./Contact";
 import Footer from "./Footer";
 import Beta from "./Beta";
 import "./index.css";
-import Config from "./config.json";
-import ReactGA from 'react-ga';
+import GoogleAnalytics from "./GoogleAnalytics";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-
-		const analyticsCode = Config.google_analytics;
-		ReactGA.initialize(analyticsCode, {
-			debug: false
-		});
-	}
-
   render() {
-	  const logPageView = () => {
-		  ReactGA.pageview(window.location.pathname + window.location.search);
-		  return null;
-	  };
-
     return (
       <div>
-	      <Route component={logPageView} />
+        <GoogleAnalytics />
         <ArraNavbar />
         <Switch>
           <Route exact path="/" component={Home} />
